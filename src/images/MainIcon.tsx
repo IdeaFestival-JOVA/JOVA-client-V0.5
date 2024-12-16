66001e88f419b2df794d1d63d3181dd6056a59bf
fakesanghyeok <s24066@gsm.hs.kr>
2024-12-16 17:23:23 +0900

    jsx 파일 tsx로 변환, 후 경로 정리

---
+++ src/images/MainIcon.tsx
@@ -0,0 +1,12 @@
+import mainLogo from "./image/mainLogo.png";
+
+const MainIcon = () => (
+  <img
+    src={mainLogo}
+    alt="main Logo" // 대체 텍스트
+    width="83.9px" // 원하는 너비
+    height="40px" // 원하는 높이
+  />
+);
+
+export default MainIcon;
---
+++ src/page/Profil/profilPicture.tsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import baseImage from "../../images/image/baseImage.png";
+import { useState } from "react";
+
+const Text = styled.h1`
+  font-family: "Pretendard-Regular", sans-serif;
+  margin: 0;
+  font-size: 36px;
+`;
+
+const ProfilWrapper = styled.div`
+  width: 687px;
+  height: 376px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 85px;
+  margin-right: 200px;
+`;
+
+const ProfilImage = styled.img`
+  width: 376px;
+  height: 376px;
+  border-radius: 50%;
+`;
+
+const ProfilInformationWrapper = styled.div`
+  width: 222px;
+  height: 376px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+const ProfilInformation = styled(Text)`
+  font-size: 24px;
+`;
+
+const ImformationWrapper = styled.div``;
+
+const UserImformation = styled(Text)`
+  font-size: 24px;
+  color: rgba(148, 148, 148, 1);
+  font-weight: 100;
+`;
+
+function ProfilPicture() {
+  const [userName, setUserName] = useState("이름");
+  const [github, setGithub] = useState("https://github.com/LeeSangHyeok0731");
+  const [mail, setMail] = useState("s20000@gsm.hs.kr");
+  const [number, setNumber] = useState("프론트엔드");
+
+  return (
+    <>
+      <Text>마이 페이지</Text>
+      <ProfilWrapper>
+        <ProfilImage src={baseImage} />
+        <ProfilInformationWrapper>
+          <ImformationWrapper>
+            <ProfilInformation>이름:</ProfilInformation>
+            <UserImformation>{userName}</UserImformation>
+          </ImformationWrapper>
+          <ImformationWrapper>
+            <ProfilInformation>github:</ProfilInformation>
+            <UserImformation>{github}</UserImformation>
+          </ImformationWrapper>
+          <ImformationWrapper>
+            <ProfilInformation>E-mail:</ProfilInformation>
+            <UserImformation>{mail}</UserImformation>
+          </ImformationWrapper>
+          <ImformationWrapper>
+            <ProfilInformation>전공:</ProfilInformation>
+            <UserImformation>{number}</UserImformation>
+          </ImformationWrapper>
+        </ProfilInformationWrapper>
+      </ProfilWrapper>
+    </>
+  );
+}
+
+export default ProfilPicture;
