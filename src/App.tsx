ce7b018565659ba896fe815f5e7b613d733dd32c
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-21 21:48:49 +0900

    구인구직 페이지 완서에 따른 routing 수정정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./page/Home/Home";
 import Notion from "./page/Notion/Notion";
 import Profil from "./page/Profil/profil";
-import JobPage from "./page/JobPage/JobPage";
+import JobVa from "./page/JobPage/JobVaPage";
 import Detail from "./page/Detail/Detail";
 import GlobalStyle from "./fonts/GlobalStyle";
 import Notification from "./page/notification/Notification";
@@ -13,7 +13,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/jobpage" element={<JobPage />} />
+        <Route path="/jobpage" element={<JobVa />} />
         <Route path="/notion" element={<Notion />} />
         <Route path="/profil" element={<Profil />} />
         <Route path="/notion/detail" element={<Detail />} />
