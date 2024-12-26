6e0f758436c74dc8ffb4861a6d2db42ff89facc4
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-26 09:26:15 +0900

    사용성없는 간격 삭제제

--- src/page/Home/contentsWrapper/RecentNotion.tsx
+++ src/page/Home/contentsWrapper/RecentNotion.tsx
@@ -1,35 +1,34 @@
 import styled from "styled-components";
 
 const Text = styled.h1`
-    font-family: 'Pretendard-Regular', sans-serif;
-    margin:0;
-    font-weight:regular;
-    font-size:20px;
-`
+  font-family: "Pretendard-Regular", sans-serif;
+  margin: 0;
+  font-weight: regular;
+  font-size: 20px;
+`;
 
 const Wrapper = styled.div`
-    width:541px;
-    height:236px;
-    background-color:white;   
-`
-
+  width: 541px;
+  height: 236px;
+  background-color: white;
+`;
 
 const NotionWrapper = styled.div`
-    width:540px;
-    height:200px;
-    background-color:white;
-    margin-top:15px;
-    border-radius:15px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`
+  width: 540px;
+  height: 200px;
+  background-color: white;
+  margin-top: 15px;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
 
-function RecentNotion(){
-    return(
-        <Wrapper>
-            <Text>구인 구직 판</Text>
-            <NotionWrapper />
-        </Wrapper>
-    )
+function RecentNotion() {
+  return (
+    <Wrapper>
+      <Text>구인구직 판</Text>
+      <NotionWrapper />
+    </Wrapper>
+  );
 }
 
 export default RecentNotion;
