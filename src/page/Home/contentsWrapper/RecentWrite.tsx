5ba0e19bdb92410c7dbb1a72feded5d276f81cc1
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-27 14:44:25 +0900

    MiniCard애 값 전달달

--- src/page/Home/contentsWrapper/RecentWrite.tsx
+++ src/page/Home/contentsWrapper/RecentWrite.tsx
@@ -1,35 +1,66 @@
 import styled from "styled-components";
+import MiniCard from "../../../components/MiniCard";
 
 const Text = styled.h1`
-    font-family: 'Pretendard-Regular', sans-serif;
-    margin:0;
-    font-weight:regular;
-    font-size:20px;
-`
+  font-family: "Pretendard-Regular", sans-serif;
+  margin: 0;
+  font-weight: regular;
+  font-size: 20px;
+`;
 
 const Wrapper = styled.div`
-    width:541px;
-    height:236px;
-    background-color:white;   
-`
-
+  width: 541px;
+  height: 236px;
+  background-color: white;
+`;
 
 const WriteWrapper = styled.div`
-    width:540px;
-    height:200px;
-    background-color:white;
-    margin-top:15px;
-    border-radius:15px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`
+  width: 540px;
+  height: 200px;
+  background-color: white;
+  margin-top: 15px;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
 
-function RecentWrite(){
-    return(
-        <Wrapper>
-            <Text>공지</Text>
-            <WriteWrapper />
-        </Wrapper>
-    )
+type NotionProps = {
+  title: string;
+  content: string;
+  category: string;
+  createdAt: string;
+  endsAt: string;
+  author: string;
+  Num?: number;
+};
+
+function RecentWrite() {
+  const NotionArray: NotionProps[] = [
+    {
+      title: "이상혁근 신이야",
+      content: "# 내가 바로 정의다",
+      category: "FRONTEND",
+      createdAt: "2024.12.1",
+      endsAt: "2024.12.31",
+      author: "이상혁혁",
+    },
+  ];
+
+  return (
+    <Wrapper>
+      <Text>공지</Text>
+      <WriteWrapper>
+        {NotionArray.map((data) => {
+          return (
+            <MiniCard
+              title={data.title}
+              author={data.author}
+              createdAt={data.createdAt}
+            />
+          );
+        })}
+      </WriteWrapper>
+    </Wrapper>
+  );
 }
 
 export default RecentWrite;
