b0a360ae5d09bd1b5adfd203f8e49f2af5bc4110
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-27 14:58:17 +0900

    플렉스 속성 수정 & 스타일 설정정

--- src/components/MiniCard.tsx
+++ src/components/MiniCard.tsx
@@ -1,16 +1,53 @@
+import styled from "styled-components";
+
 type PropsCard = {
   title: string;
   author: string;
   createdAt: string;
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const Table = styled.div`
+  width: 100%;
+  border-collapse: collapse; /* 테이블 선 겹침 제거 */
+  margin: 0; /* 테이블 자체 여백 제거 */
+`;
+
+const TableRow = styled.div`
+  display: flex;
+  border-bottom: 1px solid #e0e0e0;
+  padding: 5px 0; /* 상하 여백 최소화 */
+  line-height: 1; /* 줄 간격 최소화 */
+  &:hover {
+    background-color: #f9f9f9;
+  }
+`;
+
+const TableCell = styled.div<{ flex?: number }>`
+  flex: ${({ flex }) => flex || 1};
+  padding: 10px;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 function MiniCard({ title, author, createdAt }: PropsCard) {
   return (
-    <>
-      <p>{title}</p>
-      <p>{author}</p>
-      <p>{createdAt}</p>
-    </>
+    <Wrapper>
+      <Table>
+        <TableRow>
+          <TableCell flex={1}>{author}</TableCell>{" "}
+          <TableCell flex={1}>{title}</TableCell>
+          <TableCell flex={1}>{createdAt}</TableCell>
+        </TableRow>
+      </Table>
+    </Wrapper>
   );
 }
 
