f9698e683377606a4d6ace8665bd991e60efc6ec
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-29 00:34:28 +0900

    컴포넌트 명 수정정

---
+++ src/page/DetailJob/DetailJob.tsx
@@ -0,0 +1,26 @@
+import { useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import InfoBoxJob from "./infoBoxJob";
+import Header from "../../components/Header";
+
+function Detail() {
+  const location = useLocation();
+  const { userName, title, date, content } = location.state || {}; // 전달된 데이터 수신
+
+  return (
+    <div>
+      <Helmet>
+        <title>JOVA | {title}</title>
+      </Helmet>
+      <Header />
+      <InfoBoxJob
+        Name={userName}
+        Title={title}
+        TimeDate={date}
+        Contents={content}
+      />
+    </div>
+  );
+}
+
+export default Detail;
