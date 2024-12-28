b556c190cdc1910c36d33cfb7708dabf44bccf0d
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-28 23:58:24 +0900

    모달창 제작

--- src/page/Home/Filter.tsx
+++ src/page/Home/Filter.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const FilterWrapper = styled.div`
@@ -26,12 +27,58 @@ const Filter = styled.div`
     outline: none;
   }
 `;
+
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  padding: 20px;
+  background-color: white;
+  border: 2px solid black;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+`;
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+`;
+
+const ModalClose = styled.div``;
+
 function FilterBar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
-      <FilterWrapper>
-        <Filter>당신의 전공을 선택해 주세요 </Filter>
+      <FilterWrapper onClick={handleModal}>
+        <Filter>당신의 전공을 선택해 주세요</Filter>
       </FilterWrapper>
+      {isModalOpen && (
+        <>
+          <Overlay onClick={closeModal} />
+          <Modal>
+            <ModalClose onClick={closeModal}>x</ModalClose>
+            <h2>전공을 선택해 주세요!</h2>
+          </Modal>
+        </>
+      )}
     </>
   );
 }
