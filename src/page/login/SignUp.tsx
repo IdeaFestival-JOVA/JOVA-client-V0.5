66fbb6df6c88a5cd8bf7cad51454192d0ffa7a5b
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2024-12-31 01:18:43 +0900

    스타일 수정 & 값 입력 수정정

--- src/page/login/SignUp.tsx
+++ src/page/login/SignUp.tsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import Header from "../../components/Header";
-
-export default SignUp;
+import { useState } from "react";
+import usePostSIgnUp from "../../custom/usePostSignUp";
+import { useNavigate } from "react-router-dom";
 
 const SignUpContainer = styled.div`
+  margin-top: 150px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 20px;
   padding: 20px;
   background-color: #f9f9f9;
   border-radius: 8px;
@@ -65,63 +66,108 @@ const SignUpButton = styled.button`
   }
 `;
 
-const Wrapper = styled.div`
-  padding-top: 200px;
-  height: 100%;
+const Div = styled.div`
+  height: 170px;
 `;
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [classNum, setClassNum] = useState<number>(0);
+  const [grade, setGrade] = useState<number>(0);
+  const [number, setNumber] = useState<number>(0);
+  const [password, setPassword] = useState<string>("");
+
+  const { postSignUp } = usePostSIgnUp();
+
+  const go = useNavigate();
+
+  const handleClick = () => {
+    const data = {
+      email: email,
+      name: name,
+      classNum: classNum,
+      grade: grade,
+      number: number,
+    };
+
+    postSignUp(data);
+
+    go("/profil");
+  };
+
   return (
     <>
       <Header />
-      <Wrapper>
-        <SignUpContainer>
-          <Title>회원가입</Title>
-          <Form>
-            <InputContainer>
-              <Label htmlFor="email">이메일</Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="이메일을 입력하세요"
-              />
-            </InputContainer>
-            <InputContainer>
-              <Label htmlFor="username">성명</Label>
-              <Input
-                type="text"
-                id="username"
-                placeholder="성명을 입력하세요"
-              />
-            </InputContainer>
-            <InputContainer>
-              <Label htmlFor="password">반</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="반을을 입력하세요"
-              />
-            </InputContainer>
-            <InputContainer>
-              <Label htmlFor="passwordConfirm">학년</Label>
-              <Input
-                type="password"
-                id="passwordConfirm"
-                placeholder="학년을 입력하세요"
-              />
-            </InputContainer>
-            <InputContainer>
-              <Label htmlFor="passwordConfirm">번호</Label>
-              <Input
-                type="password"
-                id="passwordConfirm"
-                placeholder="번호를 입력하세요"
-              />
-            </InputContainer>
-            <SignUpButton>회원가입</SignUpButton>
-          </Form>
-        </SignUpContainer>
-      </Wrapper>
+      <Div />
+      <SignUpContainer>
+        <Title>회원가입</Title>
+        <Form>
+          <InputContainer>
+            <Label htmlFor="email">이메일</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="이메일을 입력하세요"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label htmlFor="username">성명</Label>
+            <Input
+              type="text"
+              id="username"
+              placeholder="성명을 입력하세요"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label htmlFor="classNum">반</Label>
+            <Input
+              type="number"
+              id="classNum"
+              placeholder="반을 입력하세요"
+              value={classNum}
+              onChange={(e) => setClassNum(Number(e.target.value))}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label htmlFor="grade">학년</Label>
+            <Input
+              type="number"
+              id="grade"
+              placeholder="학년을 입력하세요"
+              value={grade}
+              onChange={(e) => setGrade(Number(e.target.value))}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label htmlFor="number">번호</Label>
+            <Input
+              type="number"
+              id="number"
+              placeholder="번호를 입력하세요"
+              value={number}
+              onChange={(e) => setNumber(Number(e.target.value))}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label htmlFor="number">비밀번호</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="비밀번호를 입력하세요"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </InputContainer>
+          <SignUpButton onClick={handleClick}>회원가입</SignUpButton>
+        </Form>
+      </SignUpContainer>
     </>
   );
 }
+
+export default SignUp;
